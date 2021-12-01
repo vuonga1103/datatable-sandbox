46279152b66d87c0fc103ae64eee8077847fe39f
vuonga1103 <vuonga1103@yahoo.com>
2021-12-01 17:56:17 -0500

    Add custom sort for when we want x num of rows to stay static at the top always

--- src/App.js
+++ src/App.js
@@ -4,13 +4,37 @@ import DataTable from './components/DataTable'
 import FirstNameHeader from './FirstNameHeader'
 import { mockData } from './mockData'
 
-function App() {
-  const sortByLastNameLength = (rowA, rowB, columnId, desc) => {
-    return rowA.original[columnId].length > rowB.original[columnId].length
-      ? 1
-      : -1
-  }
+// Helper function to use with sort function, returns appropriate sort value if the current row should be on top
+const getSortValueForStaticRow = (numStaticRow = 1, currentRowIdx, desc) => {
+  const shouldRowBeStatic = currentRowIdx < numStaticRow
 
+  if (shouldRowBeStatic && !desc) return 1
+  if (shouldRowBeStatic && desc) return -1
+
+  return null
+}
+
+const stringSortWithStaticTwoRows = (rowA, rowB, columnId, desc) => {
+  const NUM_STATIC_ROWS = 2
+
+  // First check to see if current row (rowB) being compared should be static
+  // if it is then return the sort value that will keep it at the start of the table
+  const sortValueForStaticRow = getSortValueForStaticRow(
+    NUM_STATIC_ROWS,
+    rowB.index,
+    desc
+  )
+  if (sortValueForStaticRow) return sortValueForStaticRow
+  return rowA.original[columnId] > rowB.original[columnId] ? 1 : -1
+}
+
+// const sortByStringLength = (rowA, rowB, columnId, desc) => {
+//   return rowA.original[columnId].length > rowB.original[columnId].length
+//     ? 1
+//     : -1
+// }
+
+function App() {
   const colConfig = [
     {
       Header: FirstNameHeader,
@@ -20,15 +44,17 @@ function App() {
     {
       Header: 'Last Name',
       accessor: 'lastName',
-      sortType: React.useMemo(() => sortByLastNameLength, []),
+      sortType: React.useMemo(() => stringSortWithStaticTwoRows, []),
     },
     {
       Header: 'Email',
       accessor: 'email',
+      sortType: React.useMemo(() => stringSortWithStaticTwoRows, []),
     },
     {
       Header: 'Favorite Animal',
       accessor: 'favoriteAnimal',
+      sortType: React.useMemo(() => stringSortWithStaticTwoRows, []),
     },
   ]
 
