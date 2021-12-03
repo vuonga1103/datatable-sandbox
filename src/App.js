1cb5b79bf6427cf2c47b7dc83c4bd6acca53b847
vuonga1103 <vuonga1103@yahoo.com>
2021-12-03 10:49:56 -0500

    Add headerStyle, restructure table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import './App.css'
 import DataTable from './components/DataTable'
+import { mockData } from './mockData'
+import { IconCell } from './components/Cells/cellTypes'
+
+const COL_CONFIG = [
+  {
+    Header: 'Icon',
+    accessor: 'icon',
+    style: {
+      width: 150,
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    cellConfig: {
+      type: IconCell,
+    },
+  },
+  {
+    Header: ({ value }) => 'First Name',
+    headerStyle: { backgroundColor: 'yellow' },
+    accessor: 'firstName',
+    style: {
+      color: 'blue',
+      backgroundColor: 'yellow',
+    },
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Email',
+    accessor: 'email',
+    cellValueFormatter: value => `#${value}`,
+  },
+  {
+    Header: 'Favorite Animal',
+    accessor: 'favoriteAnimal',
+  },
+]
 
 function App() {
-  return <DataTable />
+  return <DataTable colConfig={COL_CONFIG} data={mockData} />
 }
 
 export default App
--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -2,9 +2,6 @@ import React, { useMemo } from 'react'
 import styled from 'styled-components'
 import { useTable } from 'react-table'
 
-import { mockData } from '../mockData'
-
-import { IconCell } from './Cells/cellTypes'
 import CellContainer from './CellContainer'
 
 const Styles = styled.div`
@@ -36,99 +33,59 @@ const Styles = styled.div`
   }
 `
 
-function Table({ columns, data }) {
+const DataTable = ({ colConfig, data }) => {
+  const memoizedColumns = useMemo(() => colConfig, [colConfig])
+  const memoizedData = useMemo(() => data, [data])
+
   const columnsWithCells = useMemo(() => {
-    return columns.map((column) => {
+    return memoizedColumns.map(column => {
       const columnWithCell = {
         ...column,
         Cell: CellContainer,
       }
       return columnWithCell
     })
-  }, [])
+  }, [memoizedColumns])
 
   // Use the state and functions returned from useTable to build your UI
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     useTable({
       columns: columnsWithCells,
-      data,
+      data: memoizedData,
     })
-
-  // Render the UI for your table
-  return (
-    <table {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, rowId) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell, rowCellId) => {
-                const cellId = `${rowId}-${rowCellId}`
-                return (
-                  <React.Fragment key={cellId}>
-                    {cell.render('Cell')}
-                  </React.Fragment>
-                )
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
-  )
-}
-
-const COL_CONFIG = [
-  {
-    Header: 'Icon',
-    accessor: 'icon',
-    style: {
-      width: 150,
-      display: 'flex',
-      justifyContent: 'center',
-    },
-    cellConfig: {
-      type: IconCell,
-    },
-  },
-  {
-    Header: 'First Name',
-    accessor: 'firstName',
-    style: {
-      color: 'blue',
-    },
-  },
-  {
-    Header: 'Last Name',
-    accessor: 'lastName',
-  },
-  {
-    Header: 'Email',
-    accessor: 'email',
-    cellValueFormatter: (value) => `#${value}`,
-  },
-  {
-    Header: 'Favorite Animal',
-    accessor: 'favoriteAnimal',
-  },
-]
-
-const DataTable = () => {
-  const columns = React.useMemo(() => COL_CONFIG, [])
-  const data = React.useMemo(() => mockData, [])
 
   return (
     <Styles>
-      <Table columns={columns} data={data} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()} style={column.headerStyle}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, rowId) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell, rowCellId) => {
+                  const cellId = `${rowId}-${rowCellId}`
+                  return (
+                    <React.Fragment key={cellId}>
+                      {cell.render('Cell')}
+                    </React.Fragment>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </Styles>
   )
 }
